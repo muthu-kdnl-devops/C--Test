16bef17d2e40732fadd59890cb99e033c7457b31
Muthumari <muthumariappan13399@gmail.com>
2025-07-02 07:33:55 +0530

    Update MainWindow.h

---
+++ MainWindow.h
@@ -0,0 +1,23 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QPushButton>
+#include <QLabel>
+
+class MainWindow : public QMainWindow {
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void onButtonClicked();
+
+private:
+    QPushButton *button;
+    QLabel *label;
+};
+
+#endif // MAINWINDOW_H
